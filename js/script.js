917c0b1145bcf187d542ec43e4bccd76b47de394
TalhaA <talha8877@gmail.com>
2016-07-31 02:50:44 +0300

    Color changing by class Change

--- js/script.js
+++ js/script.js
@@ -1,28 +1,13 @@
-	var car = {
-		make: 'VW',
-		type: 'Polo',
-		color: 'Blue',
-		isTurnedOn: false,
-		numberOfWheels: 4,
-		seats: ['seat 1', 'seat 2', 'seat 3', 'seat 4'],
 
-		turnOn: function () {
-			this.isTurnedOn = true;
-				},
-		fly: function (){
-			alert('fly');
-		},
-		switchCar: function (isOn) {
-			console.log('turn car' + isOn)
-			if (isOn == true) {
-				this.isTurnedOn = true;
-			} else {
-				this.isTurnedOn = false;
-			}
-		}
-		addSeat: function () {
-			this
-		}
-	};	
+var changeColor = function () {
+	var currentClassName = document.getElementbyID('cool').className;
+	
+	if (currentClassName == 'cool') {
+		document.getElementbyID('cool').className = 'cool red';
+	} else {
+		document.getElementbyID('cool').className = 'cool';
+	}
 
-	console.log("hello.there");
+}
+
+
